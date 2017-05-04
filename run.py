5458115a9fe3c3014c4745b65233a6d259c825fc
Mahendra Kalkura <mahendra@kalkura.com>
2017-05-05 01:02:27 +0530

    run.py

--- run.py
+++ run.py
@@ -1,66 +1,55 @@
-'''
-MD - \x00 - Message delimiter
-RD - \x01 - Record delimiter
-FD - \x02 - Field delimiter
-
-4 <FD> 100 <FD> <CLIENT ID>                       0xff
-4 \x02 100 \x02 C8C4048C779163D1-003B07C40000000A ?
-
-T {header<RD>} {data} D
-'''
-
+from json import loads
 from pprint import pprint
+from re import compile
 
 from requests import request
-from scrapy import Selector
 from websocket import WebSocketApp
 
+PATTERN = compile(r'document\.aip_list\.create_prebuilt_event\((.*?)\);')
 
-def matches():
+DELIMITERS_MESSAGE = '\x00'
+DELIMITERS_RECORD = '\x01'
+DELIMITERS_FIELD = '\x02'
+
+TYPES_CLIENT_ID = '4'
+TYPES_TOPIC_LOAD_MESSAGE = '\x14'
+TYPES_DELTA_MESSAGE = '\x15'
+TYPES_SUBSCRIBE = '\x16'
+TYPES_PING_CLIENT = '\x19'
+TYPES_TOPIC_STATUS_NOTIFICATION = '\x23'
+
+
+def fetch_matches():
+    matches = []
     url = 'http://sports.williamhill.com/bet/en-gb/betlive/9'
     response = request(method='GET', url=url)
     if not response:
-        return
-    selector = Selector(text=response.text)
-    trs = selector.xpath('//div[@id="ip_sport_9_types"]/div/div/div/table/tbody/tr')
-    for tr in trs:
-        url = None
-        try:
-            url = tr.xpath('.//td[3]/a/@href').extract()[0]
-        except Exception:
-            pass
-        id = url.split('/')[-2]
+        return matches
+    contents = response.text
+    contents = contents.replace('\n', '')
+    items = PATTERN.findall(contents)
+    for item in items:
+        item = loads(item)
+        id = item['event']
+        id = int(id)
         teams = {
             'home': None,
             'away': None,
         }
-        try:
-            title = tr.xpath('.//td[3]/a/span/text()').extract()[0]
-            title = title.replace('&nbsp;', '')
-            title = title.split(' v ')
-            title = map(str.strip, title)
-            title = list(title)
-            teams['home'] = title[0]
-            teams['away'] = title[1]
-        except Exception:
-            pass
-        date = None
-        status = {
-            'period': None,
-        }
-        events = None
-        timestamp = None
+        for selection in item['selections']:
+            if selection['fb_result'] == 'H':
+                teams['home'] = selection['name']
+            if selection['fb_result'] == 'A':
+                teams['away'] = selection['name']
+        date = self._get_date(item['start_time'])
         match = {
             'id': id,
             'teams': teams,
             'date': date,
-            'status': status,
-            'events': events,
-            'timestamp': timestamp, # to be updated whenever match itself is
-                                    # updated
         }
-        web_sockets(match)
-        break
+        if match['id'] and match['teams']['home'] and match['teams']['away'] and match['date']:
+            matches.append(match)
+    return matches
 
 
 def web_sockets(match):
@@ -72,59 +61,94 @@ def web_sockets(match):
         print('[+] Close')
 
     def on_message(web_socket, message):
+        global INITIALIZED
         message = bytearray(message, 'utf-8')
-        if message[0] == 52:
+        if chr(message[0]) == TYPES_CLIENT_ID:
             message = message.split(b'\x02')
             client_id = message[2]
-            print('[+] Client ID:', client_id.decode('utf-8'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/away\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/away/cards/red\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/away/cards/yellow\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/away/corners\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/away/freeKicks\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/away/goals\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/away/lineup\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/away/penalties\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/away/shots/offTarget\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/away/shots/onTarget\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/away/shots/onWoodwork\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/away/substitutions\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/away/throwIns\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/home\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/home/cards/red\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/home/cards/yellow\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/home/corners\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/home/freeKicks\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/home/goals\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/home/lineup\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/home/penalties\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/home/shots/offTarget\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/home/shots/onTarget\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/home/shots/onWoodwork\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/home/throwIns\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/homeTeamPossesion\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/mode\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/period\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/score\x01'))
-            send(bytearray(b'\x16sportsbook/football/' + bytes(match['id'], 'utf-8') + b'/stats/time\x01'))
+            client_id = client_id.decode('utf-8')
+            print('[+] Client ID:', client_id)
+            topics = [
+                'sportsbook/football/{id:d}',
+                'sportsbook/football/{id:d}/animation',
+                'sportsbook/football/{id:d}/i18n/en-gb/commentary',
+                'sportsbook/football/{id:d}/stats/away',
+                'sportsbook/football/{id:d}/stats/away/cards/red',
+                'sportsbook/football/{id:d}/stats/away/cards/yellow',
+                'sportsbook/football/{id:d}/stats/away/corners',
+                'sportsbook/football/{id:d}/stats/away/freeKicks',
+                'sportsbook/football/{id:d}/stats/away/goals',
+                'sportsbook/football/{id:d}/stats/away/lineup',
+                'sportsbook/football/{id:d}/stats/away/penalties',
+                'sportsbook/football/{id:d}/stats/away/shots/offTarget',
+                'sportsbook/football/{id:d}/stats/away/shots/onTarget',
+                'sportsbook/football/{id:d}/stats/away/shots/onWoodwork',
+                'sportsbook/football/{id:d}/stats/away/substitutions',
+                'sportsbook/football/{id:d}/stats/away/throwIns',
+                'sportsbook/football/{id:d}/stats/home',
+                'sportsbook/football/{id:d}/stats/home/cards/red',
+                'sportsbook/football/{id:d}/stats/home/cards/yellow',
+                'sportsbook/football/{id:d}/stats/home/corners',
+                'sportsbook/football/{id:d}/stats/home/freeKicks',
+                'sportsbook/football/{id:d}/stats/home/goals',
+                'sportsbook/football/{id:d}/stats/home/lineup',
+                'sportsbook/football/{id:d}/stats/home/penalties',
+                'sportsbook/football/{id:d}/stats/home/shots/offTarget',
+                'sportsbook/football/{id:d}/stats/home/shots/onTarget',
+                'sportsbook/football/{id:d}/stats/home/shots/onWoodwork',
+                'sportsbook/football/{id:d}/stats/home/substitutions',
+                'sportsbook/football/{id:d}/stats/home/throwIns',
+                'sportsbook/football/{id:d}/stats/homeTeamPossesion',
+                'sportsbook/football/{id:d}/stats/mode',
+                'sportsbook/football/{id:d}/stats/period',
+                'sportsbook/football/{id:d}/stats/score',
+                'sportsbook/football/{id:d}/stats/time',
+            ]
+            for topic in topics:
+                message = topic
+                message = message.format(id=match['id'])
+                message = TYPES_SUBSCRIBE + message + DELIMITERS_RECORD
+                message = bytearray(message, 'utf-8')
+                send(message)
             return
-        if message[0] == 25:
+        if chr(message[0]) == TYPES_TOPIC_LOAD_MESSAGE:
+            message = message[1:]
+            message = message.split(b'\x01')
+            message = map(lambda item: item.decode('utf-8'), message)
+            message = list(message)
+            print('[+] Topic Load Message:', message)
+            return
+        if chr(message[0]) == TYPES_DELTA_MESSAGE:
+            message = message[1:]
+            message = message.split(b'\x01')
+            message = map(lambda item: item.decode('utf-8'), message)
+            message = list(message)
+            print('[+] Delta Message:', message)
+            return
+        if chr(message[0]) == TYPES_PING_CLIENT:
             message = message[1:]
             message = message.split(b'\x01')
             timestamp = message[0]
-            print('[+] Ping     :', timestamp.decode('utf-8'))
-            send(bytearray(b'\x18' + timestamp + b'\x02999\x01'))
+            timestamp = timestamp.decode('utf-8')
+            print('[+] Ping:', timestamp)
+            message = TYPES_PING_CLIENT + timestamp + DELIMITERS_RECORD
+            message = bytearray(message, 'utf-8')
+            send(message)
             return
-        print('[+] Unknown  :', message)
+        if chr(message[0]) == TYPES_TOPIC_STATUS_NOTIFICATION:
+            message = message[1:]
+            message = message.split(b'\x01')
+            message = map(lambda item: item.decode('utf-8'), message)
+            message = list(message)
+            print('[+] Topic Status Notification:', message)
+        print('[+] Unknown:', repr(message))
 
     def send(message):
-        message.decode('utf-8')
-        print('[+] Send     :', message)
+        print('[+] Send:', repr(message))
         web_socket.send(message)
 
     def on_error(web_socket, error):
-        print('[+] Error    :', error)
+        print('[+] Error:', error)
 
     web_socket = WebSocketApp(
         'wss://scoreboards-ssl.williamhill.com/diffusion?v=4&ty=WB',
@@ -137,7 +161,10 @@ def web_sockets(match):
 
 
 def main():
-    matches()
+    matches = fetch_matches()
+    pprint(matches)
+    for match in matches:
+        web_sockets(match)
 
 
 if __name__ == '__main__':
